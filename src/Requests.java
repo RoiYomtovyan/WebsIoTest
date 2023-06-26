278da7b0904b761331bc8400e4f7af56f1649152
Roi Yomtovyan <37290526+RoiYomtovyan@users.noreply.github.com>
2023-06-26 12:14:00 +0300

    add test 2 code + tests description

--- src/Requests.java
+++ src/Requests.java
@@ -1,8 +1,6 @@
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 
 import org.json.JSONObject;
 
@@ -11,16 +9,12 @@ public class Requests {
     BuildRequests buildRequests= new BuildRequests();
     JSONObject responseJson ;
 
-    public JSONObject sendRequest(String api, String query,String parameters) {
+    public JSONObject sendRequest(String api, String query, String parameters) {
         try {
             HttpRequest request = buildRequests.request(api,query,parameters);
-//            HttpRequest request = HttpRequest.newBuilder()
-//                    .uri(new URI("https://api.webz.io/filterWebContent?q=(\"stock market\" OR NASDAQ) language:english&format=json&sort=crawled&ts=1595156935835&highlight=true&token=01a2ca30-a4d7-47cc-beaa-b9f321cae01e"))
-//                    .GET()
-//                    .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // Check if the response was successful (status code 200)
+            // Check if the response was successful (status code 200) - in this tests scope we are assuming we want to test only valid requests.
             if (response.statusCode() == 200) {
                 String responseBody = response.body();
                 responseJson = new JSONObject(responseBody);
--- src/Tests.java
+++ src/Tests.java
@@ -1,4 +1,7 @@
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import java.net.URLEncoder;
@@ -13,6 +16,13 @@ public class Tests {
     }
 
     @Test
+    /*
+     this test sending query that should return no results and verify there is no error and a valid response is returned:
+     test description:
+     1. send request with search parameters that should return 0 total results.
+     2. verify that the response status code is 200 (its verified in the requests class for each request we are sending)
+     3. verify that the total results value in the response is 0
+     */
     public void SanityTest01_verify_query_with_no_results() throws URISyntaxException {
         Requests requestToSend = new Requests();
         String api = "filterWebContent";
@@ -20,11 +30,45 @@ public class Tests {
         String parameters = "language:english&format=json&sort=crawled&ts=1595156935835&highlight=true";
         String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
         System.out.println(encodedQuery);
-        JSONObject request_verify_query_with_no_results = requestToSend.sendRequest(api,encodedQuery,parameters);
+        org.json.JSONObject request_verify_query_with_no_results = requestToSend.sendRequest(api,encodedQuery,parameters);
         System.out.println("request_verify_query_with_no_results " + request_verify_query_with_no_results.toString());
         int totalResults = (int) request_verify_query_with_no_results.get("totalResults");
         System.out.println("Total Results: " + totalResults);
         assert totalResults == 0;
+
+
+    }
+
+    @Test
+    /*
+     this test verifying the calculation of moreResultsAvailable in the response:
+     test description:
+     1. send valid request and get the response.
+     2. get the "posts" json array size to verify the number of posts displayed in the response.
+     3. get the total results parameter value from the results.
+     4. calculate the moreResultsAvailable value by subtraction of the posts array size from the total results.
+     5. compare the result you get in step 4 to the moreResultsAvailable parameter in the response.
+     */
+    public void SanityTest02_validate_moreResultsAvailable_value() throws URISyntaxException, ParseException {
+        Requests requestToSend = new Requests();
+        JSONParser parser = new JSONParser();
+        String api = "filterWebContent";
+        String query ="((\"stock market\" OR NASDAQ) )";
+        String parameters = "language:english&format=json&sort=crawled&ts=1595156935835&highlight=true";
+        String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
+        System.out.println(encodedQuery);
+        org.json.JSONObject request_to_verify_totalResults_in_response = requestToSend.sendRequest(api,encodedQuery,parameters);
+        System.out.println("request_to_verify_totalResults_in_response " + request_to_verify_totalResults_in_response.toString());
+        JSONObject jsonObject = (JSONObject) parser.parse(request_to_verify_totalResults_in_response.toString());
+        JSONArray threadsArray = (JSONArray) jsonObject.get("posts");
+        int threadCount = threadsArray.size();
+        System.out.println("Total Results in posts array: " + threadCount);
+        int totalResults = (int) request_to_verify_totalResults_in_response.get("totalResults");
+        System.out.println("Total Results value: " + totalResults);
+        int moreResultsAvailable = (int) request_to_verify_totalResults_in_response.get("moreResultsAvailable");
+        System.out.println("More Result Available value: " + moreResultsAvailable);
+        int totalMoreSubtraction = totalResults-moreResultsAvailable;
+        assert totalMoreSubtraction == threadCount;
 
 
     }
